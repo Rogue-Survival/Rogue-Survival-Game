84075eeaf0f5da5cee34ef628b427ac62277d3f8
Menrath-Noah <menrathn@mail.gvsu.edu>
2024-03-14 20:50:15 -0400

    Added a window of time for Player to not take damage if collided with enemy.

--- main.py
+++ main.py
@@ -54,10 +54,9 @@ xp = []
 xp_hit = []
 
 
-class Map(pygame.sprite.Sprite):
+class Map():
     # Controls map boundaries and map camera
     def __init__(self, mapX=-800, mapY=-800):
-        pygame.sprite.Sprite.__init__(self)
         self.mapX = mapX
         self.mapY = mapY
         self.cameraX = -140
@@ -114,6 +113,9 @@ class Player(pygame.sprite.Sprite):
         self.rect.width = 40
         self.gold = 0
         self.dodgeChance = 0
+        self.death = True
+        # self.playerGroup = pygame.sprite.Group()
+        # self.playerGroup.add(self.rect)
 
     def get_speed(self):
         # returns speed of player
@@ -280,10 +282,9 @@ class Player(pygame.sprite.Sprite):
                 sk.westRect.y = sk.rect.y + sk.westYVal
 
 
-class BasicAttack(pygame.sprite.Sprite):
+class BasicAttack():
     # Creates Basic Attacks for the Player
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
         self.rect = None
         self.hitBoxRect = pygame.draw.circle(transparentSurface, (255, 255, 255), (p.rect.x+19, p.rect.y+19), 78)
 
@@ -682,6 +683,7 @@ class Enemy(pygame.sprite.Sprite):
         self.skeletonKing = False
         self.enemyLength = 0
         self.enemyList = None
+        self.playerCollideCounter = 0
 
 
     def get_speed(self):
@@ -1077,7 +1079,7 @@ class Bat(Enemy):
         self.rect.x = x
         self.rect.y = y
         self.rect.width = 40
-        self.health = 215
+        self.health = 75
         self.speed = random.uniform(.6, .8)
         self.follow_mc()
         self.travel_north()
@@ -1085,153 +1087,6 @@ class Bat(Enemy):
         self.travel_south()
         self.travel_west()
         self.bat = True
-        self.playerPOS = ()
-        self.found = False
-        self.bulletRect = pygame.draw.circle(screen, (128, 12, 128), (self.rect.x, self.rect.y), 12)
-        self.bulletSpeed = 50/3
-        self.spawned2 = []
-        self.positionReached = False
-        self.goFourthA = False
-        self.goThirdA = False
-        self.goSecondA = False
-        self.goFirstA = False
-        self.angle = 0
-        self.counter = 0
-
-    def decide_action(self):
-        # time to run at player (If within a certain distance from player, then just run, ignore shooting)
-        # time to stop and shoot at player (get close enough to player to shoot but keep distance,
-        # doesn't run if too close)
-        self.shoot()
-
-    def find_angle(self):
-        if self.spawned2:
-            if not self.found:
-                self.targetX = p.rect.x+18
-                self.targetY = p.rect.y+17
-                self.startingPoint = (self.rect.x, self.rect.y)
-                self.playerPOS = (self.targetX, self.targetY)
-                self.found = True
-                self.shoot()
-
-            if self.targetX > self.startingPoint[0] and self.targetY < self.startingPoint[1]:
-                # first quadrant
-                newTriangle = pygame.math.Vector2(self.targetX - self.startingPoint[0], self.targetY -
-                                                  self.startingPoint[1])
-                self.angle = -(numpy.rad2deg(numpy.arctan(newTriangle[1]/newTriangle[0])))
-            elif self.targetX > self.startingPoint[0] and self.targetY > self.startingPoint[1]:
-                # fourth quadrant
-                newTriangle = pygame.math.Vector2(self.targetX - self.startingPoint[0], self.targetY -
-                                                  self.startingPoint[1])
-                self.angle = (numpy.rad2deg(numpy.arctan(newTriangle[1]/newTriangle[0])))
-            elif self.targetX < self.startingPoint[0] and self.targetY < self.startingPoint[1]:
-                # second quadrant
-                newTriangle = pygame.math.Vector2(self.startingPoint[0] - self.targetX, self.startingPoint[1] -
-                                                  self.targetY)
-                self.angle = (numpy.rad2deg(numpy.arctan(newTriangle[1]/newTriangle[0])))
-            elif self.targetX < self.startingPoint[0] and self.targetY > self.startingPoint[1]:
-                # second quadrant
-                newTriangle = pygame.math.Vector2(self.startingPoint[0] - self.targetX, self.startingPoint[1] -
-                                                  self.targetY)
-                self.angle = (numpy.rad2deg(numpy.arctan(newTriangle[1]/newTriangle[0])))
-
-    def shoot(self):
-        if self.spawned2:
-            if not self.found:
-                self.find_angle()
-            # print(f'b.x: {self.bulletRect.x}')
-            # print(f'b.y: {self.bulletRect.y}')
-            # print(f'p.x: {self.playerPOS[0]}')
-            # print(f'p.y: {self.playerPOS[1]}')
-            if (abs(self.bulletRect.x - self.playerPOS[0]) < 15 and abs(self.bulletRect.y - self.playerPOS[1]) < 15 or
-                    self.counter >= 75):
-                self.bulletRect.x = self.rect.x
-                self.bulletRect.y = self.rect.y
-                self.bulletValid = False
-                self.positionReached = False
-                self.goFourthA = False
-                self.goThirdA = False
-                self.goSecondA = False
-                self.goFirstA = False
-                self.found = False
-                self.counter = 0
-
-            else:
-                self.bulletValid = True
-                # if (self.bulletRect.x >= self.startingPoint[0] and self.bulletRect.y >= self.startingPoint[1]) and
-                # self.positionReached:
-                #     # fourth quadrant - continues traveling after reaching mouse position
-                #     self.bulletRect.y += math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #     self.bulletRect.x += math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #
-                # elif (self.bulletRect.x <= self.startingPoint[0] and self.bulletRect.y >= self.startingPoint[1]) and
-                # self.positionReached:
-                #     # third quadrant - continues traveling after reaching mouse position
-                #     self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #     self.bulletRect.x -= math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #
-                #
-                # elif (self.bulletRect.x <= self.startingPoint[0] and self.bulletRect.y <= self.startingPoint[1]) and
-                # self.positionReached:
-                #     # second quadrant - continues traveling after reaching mouse position
-                #     self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #     self.bulletRect.x -= math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #
-                #
-                # elif (self.bulletRect.x >= self.startingPoint[0] and self.bulletRect.y <= self.startingPoint[1]) and
-                # self.positionReached:
-                #     # first quadrant - continues traveling after reaching mouse position
-                #     self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                #     self.bulletRect.x += math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-
-                if self.bulletRect.x <= self.playerPOS[0] and self.bulletRect.y <= self.playerPOS[1] and not self.playerPOS[1] <= self.startingPoint[1] and not self.playerPOS[0] <= self.startingPoint[0] or self.goFourthA and not self.goFirstA and not self.goSecondA and not self.goThirdA:
-                    # fourth quadrant - travels to mouse position
-                    self.bulletRect.y += math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.bulletRect.x += math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.goFourthA = True
-                    # if self.playerPOS[1] - self.bulletRect.y < 10 and self.playerPOS[0] - self.bulletRect.x < 10:
-                    #     self.positionReached = True
-
-                elif self.bulletRect.x >= self.playerPOS[0] and self.bulletRect.y <= self.playerPOS[1] and not abs(self.playerPOS[0] - self.startingPoint[0]) < 12 or self.goThirdA and not self.goFirstA and not self.goSecondA and not self.goFourthA:
-                    # third quadrant - travels to mouse position
-                    self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.bulletRect.x -= math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.goThirdA = True
-                    # if self.playerPOS[1] - self.bulletRect.y <= 10 and self.bulletRect.x - self.playerPOS[0] <= 10:
-                    #     self.positionReached = True
-
-                elif (self.bulletRect.x >= self.playerPOS[0] and self.bulletRect.y >= self.playerPOS[1] or
-                      self.goSecondA and not self.goFirstA and not self.goThirdA and not self.goFourthA):
-                    # second quadrant - travels to mouse position
-                    self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.bulletRect.x -= math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.goSecondA = True
-                    # if self.bulletRect.y - self.playerPOS[1] <= 10 and self.playerPOS[0] - self.bulletRect.x <= 10:
-                    #     self.positionReached = True
-
-                elif (self.bulletRect.x <= self.playerPOS[0] and self.bulletRect.y >= self.playerPOS[1] or
-                      self.goFirstA and not self.goSecondA and not self.goThirdA and not self.goFourthA):
-                    # first quadrant - travels to mouse position
-                    self.bulletRect.y -= math.sin(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.bulletRect.x += math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                    self.goFirstA = True
-                    # if self.bulletRect.y - self.playerPOS[1] <= 10 and self.playerPOS[0] - self.bulletRect.x <= 10:
-                    #     self.positionReached = True
-
-                else:
-                    # if self.playerPOS[1] == self.startingPoint
-                    if self.playerPOS[1] >= self.startingPoint[1] and (abs(self.playerPOS[0]) -
-                                                                       self.startingPoint[0]) < 15:
-                        self.bulletRect.y += math.sin(90 * (2*math.pi/360)) * self.bulletSpeed
-                    elif self.playerPOS[1] <= self.startingPoint[1] and (abs(self.playerPOS[0]) -
-                                                                         self.startingPoint[0]) < 15:
-                        self.bulletRect.y -= math.sin(90 * (2*math.pi/360)) * self.bulletSpeed
-                    elif (abs(self.playerPOS[1] - self.startingPoint[1]) <= 5 and self.playerPOS[0] >=
-                          self.startingPoint[0]):
-                        self.bulletRect.x += math.cos(self.angle * (2*math.pi/360)) * self.bulletSpeed
-                self.bulletRect = pygame.draw.circle(screen, (128, 12, 128), (self.bulletRect.x,
-                                                                              self.bulletRect.y), 12)
-                self.counter += 1
 
 
 class skeletonKing(Enemy):
@@ -1268,6 +1123,8 @@ class skeletonKing(Enemy):
         self.targetAquired = False
         self.felled = False
         self.activate = False
+
+        self.playerCollideCounter = 0
 
     def generate_enemy(self):
         # print('1')
@@ -1413,11 +1270,19 @@ class skeletonKing(Enemy):
                         # print("ENEMY REMOVED!!!")
                         # print("----------------")
             if self.leftLegRect.colliderect(p.rect) or self.rightLegRect.colliderect(p.rect):
-                checkingDodge = random.randint(0, 100)
-                if checkingDodge <= p.dodgeChance:
-                    pass
+                if self.playerCollideCounter >= 10:
+                    checkingDodge = random.randint(0, 100)
+                    if checkingDodge <= p.dodgeChance:
+                        pass
+                    else:
+                        p.health -= 24
+                        sk.playerCollideCounter = 0
+                        print("DAMAGE TAKEN")
+                        print(p.health)
                 else:
-                    p.health -= 24
+                    self.playerCollideCounter += 1
+            else:
+                sk.playerCollideCounter = 0
 
 
 class XP(pygame.sprite.Sprite):
@@ -1440,10 +1305,9 @@ gameTimerOn = False
 # gameTime = 0
 
 
-class XP_Bar(pygame.sprite.Sprite):
+class XP_Bar():
     # Displays the XP Bar
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
         self.xpBarBorderRect = None
         self.xpBarRect = None
         self.xpBarEmptyRect = None
@@ -1498,28 +1362,6 @@ class XP_Bar(pygame.sprite.Sprite):
                         # gameTimerOn = True
                         gameTime -= tempTimer
                         self.timeAfterPause = gameTime
-
-                        """
-                        global xpB
-                        global xp
-                        global sk
-                        global ba
-                        global p
-                        global m
-                        global b
-                        xpB = XP_Bar()
-                        xp.clear()
-                        enemies.clear()
-                        bats.clear()
-                        bullets.clear()
-                        sk = skeletonKing(0,0)
-                        ba = BasicAttack()
-                        p = Player()
-                        m = Map()
-                        b = Bullet()
-                        Last thing I think is the timer to have the timer reset, which it currently does not.
-                        """
-
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             self.levelMenu = False
@@ -2051,7 +1893,7 @@ def mainMenu():
         tempTimer = pygame.time.get_ticks() - gameTime
         xpB.pauseTimer = tempTimer
         for event in pygame.event.get():
-            print(event)
+            # print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -2447,11 +2289,21 @@ while game:
                 enemy.eastRect.colliderect(p.rect) or enemy.southRect.colliderect(p.rect) or
                 enemy.westRect.colliderect(p.rect)):
             # print("COLLIDE!!!")
-            checkingDodge = random.randint(0, 100)
-            if checkingDodge <= p.dodgeChance:
-                pass
+            if enemy.playerCollideCounter >= 17:
+                checkingDodge = random.randint(0, 100)
+                if checkingDodge <= p.dodgeChance:
+                    pass
+                else:
+                    p.health -= 15
+                    enemy.playerCollideCounter = 0
+                    print("PLAYER DAMAGE TAKEN")
+                    print(p.health)
             else:
-                p.health -= 15
+                enemy.playerCollideCounter += 1
+        else:
+            enemy.playerCollideCounter = 0
+
+
 
     for bat in bats:
         if not bat.spawned:
@@ -2479,7 +2331,6 @@ while game:
                                                 (bat.rect.x + 16, bat.rect.y + 16), 1)
             # bat.generate_enemy()
             bat.follow_mc()
-            bat.spawned2.append(1)
 
             for bullet in bullets:
                 # for each active bullet, if the bullet hits a bat, deal damage if appropriate conditions met.
@@ -2519,11 +2370,18 @@ while game:
                 bat.eastRect.colliderect(p.rect) or bat.southRect.colliderect(p.rect) or
                 bat.westRect.colliderect(p.rect)):
             # print("COLLIDE!!!")
-            checkingDodge = random.randint(0, 100)
-            if checkingDodge <= p.dodgeChance:
-                pass
+            if bat.playerCollideCounter >= 17:
+                checkingDodge = random.randint(0, 100)
+                if checkingDodge <= p.dodgeChance:
+                    pass
+                else:
+                    p.health -= 16
+                    bat.playerCollideCounter = 0
+
             else:
-                p.health -= 15
+                bat.playerCollideCounter += 1
+        else:
+            bat.playerCollideCounter = 0
 
     if int(minutes) == 5 and int(seconds) == 0:
         sk.activate = True
@@ -2551,7 +2409,7 @@ while game:
                             pass
                         elif bullet not in sk.bulletCollisions and bullet.bulletValid:
                             sk.bulletCollisions.append(bullet)
-                            sk.health -= 300
+                            sk.health -= b.damage
                             # print('2')
                         i += 1
         for bullet in bullets:
@@ -2630,6 +2488,16 @@ while game:
     # if gameTime % 2 == 0:
     #     for bat in bats:
     #         bat.shoot()
+
+
+
+    # for bat in bats:
+    #     pygame.draw.rect(screen, (128,0,128), bat.rect)
+    # for enemy in enemies:
+    #     pygame.draw.rect(screen, (255,0,0), enemy.rect)
+    #
+    # pygame.draw.rect(screen, (0,255,0), p.rect)
+
     ba.attack()
     m.update_boundary()
     display_timer(gameTimeStr, timerFont, (0, 0, 0))
